daf3317da9c3c625454e0df66950a1a8944a707b
danis <ahrnadeev@gmail.com>
2024-09-27 18:54:38 +0300

    Initial Commit

--- src/main.py
+++ src/main.py
@@ -5,6 +5,7 @@
 
 # Press the green button in the gutter to run the script.
 if __name__ == "__main__":
+    #print("test")
     pass
     #translator.translator_asm.main("C:\\Users\\danis\\Desktop\\all-stuff\\lab-3-csa-test\\src\\translator\\in\\cat.asm", "C:\\Users\\danis\\Desktop\\all-stuff\\lab-3-csa-test\\src\\translator\\out\\cat_out")
 
